dc3d20fcb98f10922ffe5699df486d0213017870
Magdalena Eppens <magdaeppens@gmail.com>
2023-11-20 12:23:47 -0300

    20/11

--- java1.js
+++ java1.js
@@ -8,16 +8,27 @@ function zoomOut(elementId) {
     element.style.transform = "scale(1.0)"; 
 }
 
-
 function ChequeoTelefono() {
     var telefono = document.getElementById("telefono").value;
     if (!/^\d{10}$/.test(telefono)) {
         if (!/^\d+$/.test(telefono)) {
-            document.getElementById("mensajeTelefono").textContent = "Por favor, ingrese solo números.";
-        } else {
-            document.getElementById("mensajeTelefono").textContent = "El número no es válido. Vuelva a escribir.";
-        }
+            document.getElementById("mensajeTelefono").textContent = "Por favor, ingrese solo números.";} 
+            else {
+            document.getElementById("mensajeTelefono").textContent = "El número no es válido. Vuelva a escribir.";}
+        return false;
     } else {
         document.getElementById("mensajeTelefono").textContent = "";
+        return true;
+    }
+}
+
+function redirigirAMenu() {
+    window.location.href = "menu_pedido.html";
+}
+
+function iniciarPedido() {
+    var telefonoValido = ChequeoTelefono();
+    if (telefonoValido) {
+        redirigirAMenu();
     }
 }
